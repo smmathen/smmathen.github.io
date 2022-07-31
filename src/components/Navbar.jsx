a9dfc3719191b96d8f72f4faa834658fef2d1f88
smmathen <70829009+smmathen@users.noreply.github.com>
2022-07-31 10:05:02 -0500

    Starting social icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
@@ -12,14 +14,14 @@ const Navbar = () => {
         <img src={Logo} alt="Logo" style={{ width: "200px" }} />
       </div>
       {/*Menu*/}
-        {/* hide list on default, anything above medium screen turns into flex */}
-        <ul className="hidden md:flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
+      {/* hide list on default, anything above medium screen turns into flex */}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
 
       {/*Hamburger*/}
       <div onClick={handleClick} className="md:hidden z-10">
@@ -27,7 +29,13 @@ const Navbar = () => {
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/*Mobile Menu*/}
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Skills</li>
@@ -36,7 +44,15 @@ const Navbar = () => {
       </ul>
 
       {/*Social Icons*/}
-      <div className="hidden"></div>
+      <div className="flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li>
+            <a href="/">
+              LinkedIn <FaLinkedin />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
