37ac5a843e90a37005f2d505e05c2f29aed32d8b
smmathen <70829009+smmathen@users.noreply.github.com>
2022-08-02 19:43:11 -0500

    Adding home section of page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
-
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[#cf4f4f]">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-white">
+          Shawn Mathen
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I am a software developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I am a computer science student at Texas A&M University seeking out
+          the world of software development. Currently, I am interning at IBM as
+          a Software Developer in their Chief Information Security Office. I
+          have a strong interest in machine learning, artificial intelligence,
+          and data science, and I plan on minoring in Statistics to help me
+          explore these possibilities.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#cf4f4f] hover:border-[#cf4f4f]">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
