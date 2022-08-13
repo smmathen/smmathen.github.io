3b7075e890036db0fc88a55dfff0e674aec1746a
smmathen <70829009+smmathen@users.noreply.github.com>
2022-08-12 20:20:02 -0500

    Adding skills icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,8 +4,7 @@ import CPP from "../assets/cpp.png";
 import Java from "../assets/java.png";
 import Postman from "../assets/postman.png";
 import Elasticsearch from "../assets/elasticsearch.svg";
-import Figma from "../assets/figma.png";
-import Kibana from "../assets/kibana.png";
+import Figma from "../assets/figma.svg";
 
 const Skills = () => {
   return (
@@ -13,15 +12,39 @@ const Skills = () => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-[#cf4f4f]">
-            Experience
+            Skills
           </p>
           <p className="py-4">Here is some cool tech I have worked with!</p>
         </div>
 
-        <div>
-          <div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040C16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Python} alt="Python icon" />
-            <p>Python</p>
+            <p className="my-4">Python</p>
+          </div>
+          <div className="shadow-md shadow-[#040C16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CPP} alt="C Plus Plus icon" />
+            <p className="my-4">C++</p>
+          </div>
+          <div className="shadow-md shadow-[#040C16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Java} alt="Java icon" />
+            <p className="my-4">Java</p>
+          </div>
+          <div className="shadow-md shadow-[#040C16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Figma} alt="Figma icon" />
+            <p className="my-4">Figma</p>
+          </div>
+          <div className="shadow-md shadow-[#040C16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Postman} alt="Postman icon" />
+            <p className="my-4">Postman</p>
+          </div>
+          <div className="shadow-md shadow-[#040C16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={Elasticsearch}
+              alt="Elasticsearch icon"
+            />
+            <p className="my-4">Elasticsearch</p>
           </div>
         </div>
       </div>
