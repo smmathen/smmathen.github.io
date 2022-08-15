05661ac8ce3aa9b340c2816999775e0553801b43
smmathen <70829009+smmathen@users.noreply.github.com>
2022-08-14 20:43:47 -0500

    Fixing padding issues

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,7 @@ import AeroCheck from "../assets/aerocheck.jpg";
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto p-20 flex flex-col justify center w-full h-full">
+      <div className="max-w-[1000px] mx-auto pt-28 flex flex-col justify center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-[#cf4f4f]   ">
             Work
