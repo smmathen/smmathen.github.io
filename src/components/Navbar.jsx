e2eb59565c71a446774aaff3ede141be3bafa57a
smmathen <70829009+smmathen@users.noreply.github.com>
2022-08-19 14:24:28 -0500

    Adding cursor and send to home on logo click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,12 +12,14 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-8 bg-[#0a192f] text-gray-300">
       <div>
-        <img
-          className="transform transition-all hover:scale-125"
-          src={Logo}
-          alt="Logo"
-          style={{ width: "200px" }}
-        />
+        <Link to="home" smooth={true} duration={500}>
+          <img
+            className="transform transition-all hover:scale-125 cursor-pointer"
+            src={Logo}
+            alt="Logo"
+            style={{ width: "200px" }}
+          />
+        </Link>
       </div>
       {/*Menu*/}
       {/* hide list on default, anything above medium screen turns into flex */}
