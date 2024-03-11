769067c895ee8bc06001c87af7b93fb43efced54
Shawn Mathen <sgmathen@gmail.com>
2024-03-10 23:12:08 -0500

    Revert "transition from yarn to npm and fix form submission to clear inputs on submission"
    This reverts commit 7b5c5b5972cabc1dd59515011656916f240eb723.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,58 +1,17 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 
 const Contact = () => {
-
-  const formRef = useRef(null);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [submitting, setSubmitting] = useState(false);
-
-  const handleNameChange = (e) => setName(e.target.value);
-  const handleEmailChange = (e) => setEmail(e.target.value);
-  const handleMessageChange = (e) => setMessage(e.target.value);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-
-    const formData = new FormData(e.target);
-
-    try {
-      const response = await fetch(
-        "https://getform.io/f/20a9c515-e01a-4054-9258-788b93359aa3",
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-      if (response.ok) {
-        if (formRef.current) {
-          formRef.current.reset();
-        }
-        setName("");
-        setEmail("");
-        setMessage("");
-      } else {
-        console.error("Form submission failed");
-      }
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    } finally {
-      setSubmitting(false);
-    }
-  };
-
   return (
     <div
       name="contact"
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center pt-20"
     >
       <form
-        ref={formRef}
         method="POST"
+        action="https://getform.io/f/20a9c515-e01a-4054-9258-788b93359aa3"
         className="flex flex-col max-w-[600px] w-full"
-        onSubmit={handleSubmit}
+        target="_blank"
+        rel="noreferrer"
       >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-[#cf4f4f] text-gray-300">
@@ -64,37 +23,29 @@ const Contact = () => {
           </p>
         </div>
         <input
-          className={`bg-[#CCD6F6] p-2 placeholder-[#0a192f] ${name ? "text-black" : "text-gray-500"
-            }`}
+          className="bg-[#CCD6F6] p-2 placeholder-[#0a192f]"
           type="text"
           placeholder="John Doe"
           name="name"
-          value={name}
-          onChange={handleNameChange}
           required
         />
         <input
-          className={`my-4 p-2 bg-[#CCD6F6] placeholder-[#0a192f] ${email ? "text-black" : "text-gray-500"
-            }`}
+          className="my-4 p-2 bg-[#CCD6F6] placeholder-[#0a192f]"
           type="email"
           placeholder="email@domain.com"
+          pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
           name="email"
-          value={email}
-          onChange={handleEmailChange}
           required
         />
         <textarea
-          className={`bg-[#CCD6F6] p-2 placeholder-[#0a192f] ${message ? "text-black" : "text-gray-500"
-            }`}
+          className="bg-[#CCD6F6] p-2 placeholder-[#0a192f]"
           name="message"
           rows="10"
           placeholder="Enter your message here!"
-          value={message}
-          onChange={handleMessageChange}
           required
         ></textarea>
-        <button disabled={submitting} className="text-white border-2 hover:bg-[#cf4f4f] hover:border-[#cf4f4f] px-8 py-3 my-8 mx-auto flex items-center">
-          {submitting ? "Submitting..." : "Submit!"}
+        <button className="text-white border-2 hover:bg-[#cf4f4f] hover:border-[#cf4f4f] px-8 py-3 my-8 mx-auto flex items-center">
+          Submit!
         </button>
       </form>
     </div>
